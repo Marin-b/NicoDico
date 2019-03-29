310e28dc54655f85a629640be95eb2fb7e384d60
Marin Bibot <Marin.bibot@hotmail.be>
2019-03-29 17:44:38 +0100

    hello

--- app/javascript/functions/exempleShow.js
+++ app/javascript/functions/exempleShow.js
@@ -1,17 +1,17 @@
-const switchOpacity = () => {
+// const switchOpacity = () => {
 
-}
+// }
 
 
-const exempleShow = () => {
-  const seeMore = document.querySelectorAll('.seeMore')
-  if (seeMore.length > 0) {
-    seeMore.forEach((btn) => {
-      seeMore.addEventListener("click", (event) => {
-        switchOpacity();
-      };
-    });
-  };
-};
+// const exempleShow = () => {
+//   const seeMore = document.querySelectorAll('.seeMore')
+//   if (seeMore.length > 0) {
+//     seeMore.forEach((btn) => {
+//       seeMore.addEventListener("click", (event) => {
+//         switchOpacity();
+//       };
+//     });
+//   };
+// };
 
-export { exempleShow };
+// export { exempleShow };
