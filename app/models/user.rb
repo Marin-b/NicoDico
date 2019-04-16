b5e3626b6bd8f29c58283d1d066824e9804ce71f
Marin Bibot <Marin.bibot@hotmail.be>
2019-04-16 13:46:19 +0200

    fixed facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,29 +7,23 @@ class User < ApplicationRecord
   has_many :nuances
   has_many :listes, dependent: :destroy
 
-   def self.find_for_facebook_oauth(auth)
-    user_params = auth.slice(:provider, :uid)
-    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+  def self.find_for_facebook_oauth(auth)
+    user_params = { email: auth.info[:email], first_name: auth.info[:first_name], last_name: auth.info[:last_name], uid: auth.info[:uid], provider: auth.info[:provider] }
     user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
     user_params = user_params.to_h
-    puts "before looking for user"
 
     user = User.find_by(provider: auth.provider, uid: auth.uid)
     user ||= User.find_by(email: auth.info.email) # User did a regular sign up in the past.
-    puts "after looking for user"
     puts user_params
     if user
       user.update(user_params)
     else
-      puts "before creating new user"
       user = User.new(user_params)
       puts user
-      user.password = Devise.friendly_token[0,20] # Fake password for validation
-      puts "before saving new user"
+      user.password = Devise.friendly_token[0,20]
       user.save
-      puts "after saving new user"
     end
 
     return user
